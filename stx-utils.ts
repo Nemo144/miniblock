023669901fa0829baa32409651553eef0d6f6968
Nnamdi Okeke <onnamdialex@gmail.com>
2025-07-14 17:45:59 +0100

    :bulb: Add comments for better clarity to the abbreviate-functions

--- stx-utils.ts
+++ stx-utils.ts
@@ -1,9 +1,11 @@
 //function to abbreviate addresses
 export function abbreviateAddress(address: string) {
+  //taking the first 5 and the last 5
   return `${address.substring(0, 5)}...${address.substring(36)}`;
 }
 
 //function to abbreviate the transaction Id
 export function abbreviateTxnId(txnId: string) {
+  //taking the first 5 and the last 5
   return `${txnId.substring(0, 5)}...${txnId.substring(62)}`;
 }
